075c47367966929a92435fcdf3c647b187a2c002
Akaash Venkat <akaashvenkat@gmail.com>
2019-11-14 00:05:36 -0800

    Add token verification

--- tradespace_backend/src/app.py
+++ tradespace_backend/src/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, g
+from flask_httpauth import HTTPTokenAuth
 from UsersAPI import users_api
 from LoginAPI import login_api
 from ItemsAPI import items_api
 from SearchAPI import search_api
 import firebase_admin
+from firebase_admin import auth as firebase_auth
 from firebase_admin import credentials
 
 cred = credentials.Certificate("../instance/tradespace_firebase_admin_key.json")
@@ -15,9 +17,25 @@ app.register_blueprint(login_api,  url_prefix='/login')
 app.register_blueprint(items_api,  url_prefix='/items')
 app.register_blueprint(search_api, url_prefix='/search')
 
+auth = HTTPTokenAuth(scheme='Token')
+
+@auth.verify_token
+def verify_token(token):
+    try:
+        decoded_token = firebase_auth.verify_id_token(id_token)
+        g.uid = decoded_token['uid']
+        return True
+    except:
+        return False
+
 @app.route("/")
 def hello():
   return "Hello world!"
+
+@app.route("/authreq")
+@auth.login_required
+def authreq():
+    return "blah"
 
 if __name__ == "__main__":
   app.run()
